b9082570063cb24cac1e5e2f2fd9bccb75efa10d
tleis4 <146903205+tleis4@users.noreply.github.com>
2023-11-05 19:31:07 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
        User one = new User("Katie", "Tech", 42);
 one.output();
+       PersonalityQuiz quiz = new PersonalityQuiz quiz;
+        String question = quiz.getQuestion();
+        String answer = quiz.getAnswer();
+        System.out.println("Question: " + question);
     }
 
 }

7eec74cd01d546b29300cb7cb35defcd9b780bd7
katiedelucio <125385457+katiedelucio@users.noreply.github.com>
2023-11-08 19:25:29 -0500

    Update User.java comments

--- User.java
+++ User.java
@@ -1,46 +1,47 @@
+//author: Katie DeLucio
 
 public class User {
 String name, occupation;
 int age;
-
-public User(String name, String occupation, int age) {
     
+//constructor
+public User(String name, String occupation, int age) { 
     this.name = name;
     this.age = age;
     this.occupation = occupation;
 }
-
+//default constructor
 public User() {
    name = "";
    occupation = "";
    age = 0;
 }
-
+//getter name
   public String getName() {
     return name;
 }
-
+//setter name
 public void setName(String name) {
     this.name = name;
 }
-
+getter occupation
 public String getOccupation() {
     return occupation;
 }
-
+setter occupation
 public void setOccupation(String occupation) {
     this.occupation = occupation;
 }
-
+//getter age
 public int getAge() {
     return age;
 }
-
+//setter age
 public void setAge(int age) {
     this.age = age;
 }
   
-
+//print user info
 public void output() {
     System.out.println("User details: \nName = " + name + "\nOccupation = " + occupation + "\nAge = " + age);
 }

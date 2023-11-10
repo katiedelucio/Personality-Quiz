7f18c9a6df13add574950ef7d2920923cfbe631f
tleis4 <146903205+tleis4@users.noreply.github.com>
2023-11-10 10:20:21 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,17 @@ one.output();
         String question = quiz.getQuestion();
         String answer = quiz.getAnswer();
         System.out.println("Question: " + question);
+
+        quiz.setQuestion("1.");
+        quiz.setQuestion("2.");
+        quiz.setQuestion("3.");
+        quiz.setQuestion("4.");
+        quiz.setQuestion("5.");
+        quiz.setQuestion("6.");
+        quiz.setQuestion("7.");
+        quiz.setQuestion("8.");
+        quiz.setQuestion("9.");
+        quiz.setQuestion("10.");
     }
 
 }

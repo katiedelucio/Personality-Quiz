408715fdc75f31003f615b41b3bb23927f369ca8
tleis4 <146903205+tleis4@users.noreply.github.com>
2023-11-10 10:34:54 -0500

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,30 +1,47 @@
-//PersonalityQuiz, Tiffany Leister, 11/10/2023
-//Class Quiz that contains the question and answer attributes to later display for user interface 
+/*PersonalityQuiz, Tiffany Leister, 11/10/2023
+Class Quiz that contains the question and answerchoice attributes to later display for user interface*/ 
 
 public class Quiz{
     String question;
-    Char answer;
-
-public PersonalityQuiz(String question)
+    Char[] answer;
+    
+//Constructor
+public Quiz(String question, char[] answer)
 {
     this.question = question;
     this.answer = answer;
  }
-public void setQuestion(String question)
+    
+//Default Constructor
+public Quiz()
     {
-        this.question = question;
+        this.question = "";
+        this.answer = new char[0];
     }
+    
+//getter Question
 public String getQuestion()
 {
 return question;
 }
-public void setAnswer(Char answer)
-{
-this.answer = answer;
-}
+    
+//setter Question
+public void setQuestion(String question)
+    {
+        this.question = question;
+    }
+    
+//getter Answer
 public char getAnswer()
 {
 return answer;
 }
+    
+//setter Answer
+public void setAnswer(Char[] answer)
+{
+this.answer = answer;
+}
+
 
     

79ace8a73823eabb755740ce9039c1df1435aae2
madelinekabbott <makiabbo@iu.edu>
2023-11-12 14:57:30 -0500

    added comments and missing elements to my class

--- PersonalityQuiz.java
+++ PersonalityQuiz.java
@@ -7,29 +7,37 @@ public class PersonalityQuiz{
     String question;
     char answer;
     
+    // default constructor
     public PersonalityQuiz() {
     	question = "";
     	answer = 0;
     }
 
-public PersonalityQuiz(String question)
-{
+    // constructor with values
+    public PersonalityQuiz(String question, char answer)
+    {
     this.question = question;
- }
+    this.answer = answer;
+    }
+    
+    // Set and get methods for PersonalityQuiz question attributes
+    public void setQuestions(String question)
+    {
+    	this.question = question;
+    }
+    public String getQuestion()
+    {
+    	return question;
+    }
 
-public String getQuestion()
-{
-return question;
-}
+    public void setAnswer(char answer)
+    {
+    	this.answer = answer;
+    }
 
-public void setAnswer(char answer)
-{
-this.answer = answer;
-}
-
-public char getAnswer()
-{
-return answer;
-}
+    public char getAnswer()
+    {
+    	return answer;
+    }
 
 }

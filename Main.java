34e8e7be33f28bfe257d03b35820289065964b47
katiedelucio <125385457+katiedelucio@users.noreply.github.com>
2023-11-20 17:02:33 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
-
+import java.util.Scanner;
 public class Main {
-
+public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        User one = new User("Katie", "Tech", 42);
+        User one = new User();
+        System.out.println ("What is your name?");
+        String name = scan.nextLine();
+        one.setName(name);
+        System.out.println("What is your occupation?");
+        String job = scan.nextLine();
+        one.setOccupation(job);
+        System.out.println("What is your age?");
+        int age = scan.nextInt();
+        one.setAge(age);
         one.output();
+        
         Quiz quiz = new Quiz();
         String question = quiz.getQuestion();
-        char answer = quiz.getAnswer();
+        char[] answer = quiz.getAnswer();
         System.out.println("Question: " + question);
 
         quiz.setQuestion("1.");

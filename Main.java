57d624b8de975862995e0bf137adc24484837a1c
tleis4 <146903205+tleis4@users.noreply.github.com>
2023-11-21 09:38:43 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,16 +19,16 @@ public static Scanner scan = new Scanner(System.in);
         char[] answer = quiz.getAnswer();
         System.out.println("Question: " + question);
 
-        quiz.setQuestion("1.");
-        quiz.setQuestion("2.");
-        quiz.setQuestion("3.");
-        quiz.setQuestion("4.");
-        quiz.setQuestion("5.");
-        quiz.setQuestion("6.");
-        quiz.setQuestion("7.");
-        quiz.setQuestion("8.");
-        quiz.setQuestion("9.");
-        quiz.setQuestion("10.");
+        quiz.setQuestion("1. Do you enjoy social gatherings or large crowds?");
+        quiz.setQuestion("2. Are you comfortable initiating conversations with strangers?");
+        quiz.setQuestion("3. Do you make friends easily?");
+        quiz.setQuestion("4. Would your friends decribe you as reserved?");
+        quiz.setQuestion("5. Would you rather communicate through text or emails than over the phone?");
+        quiz.setQuestion("6. Are your weekends typically booked with plans?");
+        quiz.setQuestion("7. Do you prefer teamwork over individual tasks?");
+        quiz.setQuestion("8. Do you have reservations when expressing your opinions?");
+        quiz.setQuestion("9. Do you feel as if you have a social battery?");
+        quiz.setQuestion("10. Are you more open in conversations?");
     }
 
 }

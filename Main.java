deaa87940cc7006473c811db23b55d270d3aa7df
katiedelucio <125385457+katiedelucio@users.noreply.github.com>
2023-11-22 16:32:26 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
-public static Scanner scan = new Scanner(System.in);
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         User one = new User();
-        System.out.println ("What is your name?");
+
+        System.out.println("What is your name?");
         String name = scan.nextLine();
         one.setName(name);
         System.out.println("What is your occupation?");
@@ -11,24 +15,56 @@ public static Scanner scan = new Scanner(System.in);
         one.setOccupation(job);
         System.out.println("What is your age?");
         int age = scan.nextInt();
+        scan.nextLine();
         one.setAge(age);
         one.output();
-        
+
         Quiz quiz = new Quiz();
-        String question = quiz.getQuestion();
-        char[] answer = quiz.getAnswer();
-        System.out.println("Question: " + question);
+
+        String answer;
+        ArrayList<String> answers = new ArrayList<String>();
 
         quiz.setQuestion("1. Do you enjoy social gatherings or large crowds?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("2. Are you comfortable initiating conversations with strangers?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("3. Do you make friends easily?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("4. Would your friends decribe you as reserved?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("5. Would you rather communicate through text or emails than over the phone?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("6. Are your weekends typically booked with plans?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("7. Do you prefer teamwork over individual tasks?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("8. Do you have reservations when expressing your opinions?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("9. Do you feel as if you have a social battery?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
         quiz.setQuestion("10. Are you more open in conversations?");
+        System.out.println("Question: " + quiz.getQuestion());
+        answer = scan.nextLine();
+        answers.add(answer);
+        System.out.println(answers);
     }
 
 }
